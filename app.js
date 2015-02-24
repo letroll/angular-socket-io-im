a7a45aabf4c12b86fc85b75c45527e93fea0afb2
letroll <julien.quievreux@gmail.com>
2015-02-24 18:12:04 +0100

    maj

--- app.js
+++ app.js
@@ -5,49 +5,40 @@
 
 var express = require('express'),
   routes = require('./routes'),
-  socket = require('./routes/socket.js');
+  socket = require('./routes/socket.js'),
+  http   = require('http'),
+  path   = require('path');
 
-var app = module.exports = express.createServer();
+var app = module.exports = express();
 
 // Hook Socket.io into Express
-var io = require('socket.io').listen(app);
+var io = require('socket.io');
 
 // Configuration
+app.set('port', process.env.PORT || 3000);
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(app.router);
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.set('view options', {
-    layout: false
-  });
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.static(__dirname + '/public'));
-  app.use(app.router);
-});
-
-app.configure('development', function(){
+// development only
+if (app.get('env') === 'development') {
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
+}
 
-app.configure('production', function(){
+// production only
+if (app.get('env') === 'production') {
   app.use(express.errorHandler());
-});
-
-// Routes
-
-app.get('/', routes.index);
-app.get('/partials/:name', routes.partials);
-
-// redirect all others to the index (HTML5 history)
-app.get('*', routes.index);
+};
 
 // Socket.io Communication
-
+http = http.createServer(app);
+io = io.listen(http);
 io.sockets.on('connection', socket);
 
 // Start server
 
-app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+http.listen(app.get('port'), function(){
+  console.log("Express server listening on port %d in %s mode", app.get('port'), app.get('env'));
 });
